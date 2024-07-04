3a29cf9cd805a87cd1d988931270352079d56cfa
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-04 23:18:49 +0000

    update LinkedList

--- LinkedList/SinglyLinkedList/Python/LinkedList.py
+++ LinkedList/SinglyLinkedList/Python/LinkedList.py
@@ -1,3 +1,12 @@
+from Node import Node
+
 class LinkedList:
     def __init__(self):
         self.__head = None
+        self.__count = 0
+
+    def getHead(self):
+        return self.__head
+    
+    def getNodeCount(self):
+        return self.__count
